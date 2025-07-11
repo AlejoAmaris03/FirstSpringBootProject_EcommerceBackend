513728cf8564c37fe10c668a395449d3ea3e21af
AlejoAmaris03 <alejoamaris2005@gmail.com>
2025-07-11 10:38:05 -0500

    Refactor: Adding admin acc. to DataInitializer

--- src/main/java/com/springboot/ecommerce_backend/config/DataInitializer.java
+++ src/main/java/com/springboot/ecommerce_backend/config/DataInitializer.java
@@ -5,15 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import com.springboot.ecommerce_backend.models.Category;
 import com.springboot.ecommerce_backend.models.Role;
+import com.springboot.ecommerce_backend.models.User;
 import com.springboot.ecommerce_backend.repositories.CategoryRepository;
 import com.springboot.ecommerce_backend.repositories.RoleRepository;
+import com.springboot.ecommerce_backend.repositories.UserRepository;
+import com.springboot.ecommerce_backend.services.UserService;
 
 @Component
 
 public class DataInitializer {
     @SuppressWarnings("unused")
     @Bean
-    public CommandLineRunner initRoles(RoleRepository roleRepo, CategoryRepository categoryRepo) {
+    public CommandLineRunner initRoles(RoleRepository roleRepo) {
         return roles -> {
             // Default roles
             if(roleRepo.findByName("ROLE_ADMIN") == null)
@@ -21,7 +24,13 @@ public class DataInitializer {
                 
             if(roleRepo.findByName("ROLE_CUSTOMER") == null)
                 roleRepo.save(new Role(0, "ROLE_CUSTOMER"));
+        };
+    }
 
+    @SuppressWarnings("unused")
+    @Bean 
+    public CommandLineRunner initCategories(CategoryRepository categoryRepo) {
+        return cat -> {
             // Default categories
             if(categoryRepo.findByName("Pants") == null)
                 categoryRepo.save(new Category(0, "Pants"));
@@ -31,6 +40,25 @@ public class DataInitializer {
 
             if(categoryRepo.findByName("Sportswear") == null)
                 categoryRepo.save(new Category(0, "Sportswear"));
+        };
+    }
+
+    @SuppressWarnings("unused")
+    @Bean 
+    public CommandLineRunner initUsers(UserRepository userRepo, UserService userService) {
+        return cat -> {
+            // Default admin acc
+            if(userRepo.findUserByUsername("admin") == null) {
+                userService.registerUser(new User(
+                    0,
+                    "Admin",
+                    "Surname",
+                    "admin@example.com",
+                    "admin",
+                    "123",
+                    new Role(1, "ROLE_ADMIN")
+                ), "register");
+            }
         };
     }
 }
